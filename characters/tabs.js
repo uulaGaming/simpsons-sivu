47b2ea9b4ac86bda61d3a7c7c206aa0342901ba8
Uula <us3411@edu.turku.fi>
2026-05-18 13:13:52 +0300

    bart funnies

--- characters/tabs.js
+++ characters/tabs.js
@@ -1,6 +1,10 @@
 const tabs = document.getElementsByClassName('tab');
 const tabbtns = document.getElementsByClassName('tabbtn');
 
+const mview = document.getElementById('memeviewer');
+const mimg = document.getElementById('viewimg')
+const mlist = document.getElementById('memelist');
+
 function openTab(e, tab) {
   for (const elem of tabbtns) {
     elem.className = elem.className.replace(" active", "");
@@ -10,4 +14,15 @@ function openTab(e, tab) {
   }
   document.getElementById(tab).style.display = "block";
   e.currentTarget.className += " active";
+}
+
+function openMeme(e) {
+  mimg.src = e.currentTarget.src;
+  mlist.style.display = "none";
+  mview.style.display = "block";
+}
+
+function memelist() {
+  mlist.style.display = "block";
+  mview.style.display = "none";
 }
